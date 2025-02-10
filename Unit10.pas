2fcb401718f11d2b21fc532af13082f950a74bdb
fiedelbambu <christianfiedler4@t-online.de>
2025-02-10 09:47:20 +0100

    Initial commit

---
+++ Unit10.pas
@@ -0,0 +1,24 @@
+unit Unit10;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm10 = class(TForm)
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form10: TForm10;
+
+implementation
+
+{$R *.dfm}
+
+end.
